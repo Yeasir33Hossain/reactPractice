037cff5ba6710d82f2fdacd60ff561fb4d9efaab
Yeasir <47098685+Yeasir33Hossain@users.noreply.github.com>
2024-05-12 18:19:48 +0600

    TicTacTow game Develop

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,11 +1,13 @@
 // components/MainContent.js
 import React from 'react';
 import JsonDataPage from './JsonDataPage';
+import Board from './TicTacToe';
 function MainContent() {
   return (
     <main>
       <h2>Main Content</h2>
       <p>Welcome to the dashboard!</p>
+      <Board/>
       <JsonDataPage />
     </main>
   );
